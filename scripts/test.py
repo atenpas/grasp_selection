b8a7f38238dcc5c499f559993b3b1798613a0902
atp <andreas.tenpas@gmail.com>
2015-07-20 17:47:10 -0400

    remove test.py

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import openravepy
-
-env = openravepy.Environment()
-env.StopSimulation()
-#env.Load('/home/baxter/baxter_ws/src/sensor_explorer/openravestuff/manipulator_baxter_structure.xml')
-openrave_root = '/home/baxter/baxter_ws/src/grasp_selection/openrave/'
-env.Load(openrave_root + "manipulator_baxter_structure.xml")
